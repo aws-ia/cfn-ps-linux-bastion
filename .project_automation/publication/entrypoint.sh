a1340f1498659a0a23205ce7cd6a42357f47a529
IA Automator <dev@null>
2023-05-06 14:29:16 +0000

    (automated) Updates from project type

--- .project_automation/publication/entrypoint.sh
+++ .project_automation/publication/entrypoint.sh
@@ -4,3 +4,12 @@
 # managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
 PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+
+# Add execute permission and run publishing script s3_publish.sh
+if [ -n "${BASE_PATH}" ]
+then
+    chmod +x $PROJECT_PATH"/.project_automation/publication/s3_publish.sh"
+    $PROJECT_PATH"/.project_automation/publication/s3_publish.sh"
+else
+  echo "Local build mode (skipping publishing)"
+fi
