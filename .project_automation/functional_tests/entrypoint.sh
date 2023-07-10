b467871a2d247eda3dc97058e1a416981d140c07
IA Automator <dev@null>
2023-07-10 17:45:40 +0000

    (automated) Updates from project type

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -5,6 +5,33 @@
 PROJECT_PATH=${BASE_PATH}/project
 PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 
+printf '\nFunctional test...\n'
+
+DOCS_BRANCH="html-guide"
+## Check if docs/ files are modified?
+git fetch
+git branch
+DIFF_OUTPUT=$(git diff HEAD..origin/main)
+## If docs are modified, render updated index.html file and
+## create a PR with index.html file.
+if echo "${DIFF_OUTPUT}" | grep "^diff --git a/docs/"; then
+    printf '\nChanges detected in the /docs files. \n'
+    #--- Github pages site generation ---#
+    asciidoctor --version
+    # Generate guide - filename -> index.html
+    asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --doctype=book -a toc2 -a production_build docs/boilerplate/index_deployment_guide.adoc
+    ## Create PR with index.html file
+    CURRENT_BRANCH=$(git branch --show-current)
+    git checkout main
+    git checkout -b "${DOCS_BRANCH}"
+    git add index.html
+    git commit -m '(automated) rendered html deployment guide'
+    git push --set-upstream origin "${DOCS_BRANCH}"
+    gh pr create --title 'Generated deployment guide' --body "_This is an automated PR with rendered html file for the deployment guide. Please review it before merge_"
+else
+    printf '\nNo changes detected in the /docs files. \n'
+fi  
+
 ##----------------------------------------------------
 ## Download taskcat overrides from AWS Secrets Manager
 ## ---------------------------------------------------
